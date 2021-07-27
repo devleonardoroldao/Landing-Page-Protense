d376a5852a0df0d5fd3d4113e132842103165db3
Leonardo Roldão <41723429+veloxsite@users.noreply.github.com>
2021-07-27 06:34:21 -0300

    Subindo landing page

---
+++ _formulario/alterar_esse_arquivo.php
@@ -0,0 +1,7 @@
+<?php 
+$smtp_endereco = "mail.sitevelox.com.br";
+$seu_email = "leonardo@sitevelox.com.br";
+$usuario_smtp = "leonardo@sitevelox.com.br";
+$senha_smtp = "1a73zaP02@2017"; 
+$mensagem_sucesso = "Enviado com sucesso!";
+?>
---
+++ index.php
@@ -0,0 +1,464 @@
+<html>
+<head>
+<meta charset="utf-8">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>Criar Site Barato - SiteVelox</title>
+<link rel="stylesheet" href="css/font-awesome.min.css" type="text/css" />
+<meta name="viewport" content="width=device-width,minimum-scale=1,initial-scale=1, maximum-scale=1.0, user-scalable=0">
+<link rel="stylesheet" href="css/normalize.css" type="text/css">
+<script src="https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js" type="text/javascript"></script>
+<script type="text/javascript">WebFont.load({google:{families:["Open Sans:300,300italic,400,400italic,600,600italic,700,700italic,800,800italic"]}});</script>
+</head>
+<body class="body">
+<header id="topo" class="topo">
+<div class="limite-site">
+<a href=""> <img src="images/sitevelox.png" width="300" alt=""> </a>
+</div>
+</header>
+
+<!-- otimizacao01 -->
+<div class="box-hedline" style="background-image:linear-gradient(180deg,rgba(5,19,27,0.63),rgba(5,19,27,0.63)),url(https://www.galaxcms.com.br/imgs_crud_comum/3316/Sites-com-o-melhor-preco-do-Br-20200107095706.jpg)">
+<div class="limite-site">
+<div class="w-row">
+<div class="col-reset w-col w-col-2 w-col-stack"></div>
+<div class="col-reset w-col w-col-8 w-col-stack">
+<div class="bloco-1">
+<h1 class="h2 h3">Título
+<font color="#96c11f">40%</font> OFF </h1>
+<p class="sub">* descrição > <u>só hoje!</u> <br /> <br />
+<div style="background-color:#96c11f;color:#fff" class="bt-padrao versao-2 button-cta w-button" data-ix="pop-up-orcamento">Pegar Desconto</div>
+</p>
+</div>
+</div>
+<div class="col-reset w-col w-col-2 w-col-stack"></div>
+</div>
+</div>
+</div>
+<!-- end -->
+
+<span id="idLanding" class="none" data-id="6"></span>
+
+<main class="corpo">
+
+<!-- otimizacao02 -->
+<section class="secao s-1">
+<div class="limite-site">
+<div class="w-row">
+<div class="col-reset w-col w-col-3 w-col-stack"></div>
+<div class="col-reset w-col w-col-6 w-col-stack">
+<h3 class="h2 h-chamada">Título <br />
+<div style="background-color:#40a8df;color:#fff" class="bt-padrao versao-2 button-cta w-button" data-ix="pop-up-orcamento">Quero um site profissional</div>
+</h3>
+</div>
+<div class="col-reset w-col w-col-3 w-col-stack"></div>
+</div>
+</div>
+</section>
+<!-- end -->
+
+<!-- otimizacao03 -->
+<section class="secao s2">
+<div class="limite-site">
+<div class="row-1 w-row">
+<div class="col-reset w-col w-col-7 w-col-stack"> <img src="https://www.galaxcms.com.br/up_crud_comum/3316/irritado-computador-20200107112617.png" alt="" class="image"> </div>
+<div class="col-reset w-col w-col-5 w-col-stack">
+<div class="bloco-texto">
+<h2 class="h2">título</h2>
+<p class="sub">conteúdo<br />
+<div style="background-color:#40a8df;color:#FFF" class="bt-padrao versao-2 button-cta w-button" data-ix="pop-up-orcamento">Quero Orçamento</div>
+</p>
+</div>
+</div>
+</div>
+</div>
+</section>
+<!-- end -->
+
+
+<!-- otimizacao04 -->
+<section class="secao s-1">
+<div class="limite-site">
+<div class="w-row">
+<div class="col-reset w-col w-col-3 w-col-stack"></div>
+<div class="col-reset w-col w-col-6 w-col-stack">
+<h2 class="h2 h-chamada">título <br />
+<div style="background-color:#fff;color:#96c11f" class="bt-padrao versao-2 button-cta w-button" data-ix="pop-up-orcamento">Quero orçamento</div>
+</h2>
+</div>
+<div class="col-reset w-col w-col-3 w-col-stack"></div>
+</div>
+</div>
+<div class="barra-lado _2"></div>
+<div class="barra-lado"></div>
+</section>
+<!-- end -->
+
+
+<!-- otimizacao 05 -->
+<section class="secao s4">
+<div class="limite-site">
+<div class="row-2 w-row">
+<div class="col-reset w-col w-col-5 w-col-stack">
+<div class="bloco-texto _2">
+<h2 class="h2">título</h2>
+<p class="sub">conteúdo<br />
+<div style="background-color:#96c11f;color:#FFF" class="bt-padrao versao-2 button-cta w-button" data-ix="pop-up-orcamento">Orçamento Econômico</div>
+</p>
+</div>
+</div>
+<div class="col-reset w-col w-col-7 w-col-stack">
+<div class="w-embed-youtubevideo"> <iframe class="iframe-youtube" src="https://www.youtube.com/embed/UCfNip5lF1eEutTt-skja6iQ?autoplay=0&rel=0" allowfullscreen></iframe> </div>
+</div>
+</div>
+</div>
+</section>
+<!-- end -->
+
+<!-- otimizacao06 -->
+<section class="secao s4 _2">
+<div class="limite-site">
+<h2 class="h2 t-c">Título depoimentos</h2>
+<div class="row w-row">
+<div class="col-reset w-col w-col-tiny-12 w-col-small-12 w-col-4 w-col-medium-6 w-col-stack">
+<div class="link-depoimento w-inline-block">
+<div class="w-embed-youtubevideo">
+<div class="w-embed-youtubevideo-depo"> <iframe class="iframe-youtube" src="https://www.youtube.com/embed/UCfNip5lF1eEutTt-skja6iQ?autoplay=0&rel=0" allowfullscreen></iframe> </div>
+</div>
+<div class="depoimento-texto">
+<p>texto</p>
+</div>
+<div class="info-cliente">
+<div class="w-row">
+<div class="w-col w-col-3">
+<div class="foto-cliente" style="background-image:url(https://www.galaxcms.com.br/imgs_crud_comum/3316/Diogo-20191203115404.jpg)"></div>
+</div>
+<div class="w-col w-col-9">
+<div class="bloco-info-cliente">
+<h3 class="titulo-de">Diogo</h3>
+<div>Diretor Geral na Affiar</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div> <span class="clear-767 row w-row"></span><span class="clear-479 row w-row"></span>
+<div class="col-reset w-col w-col-tiny-12 w-col-small-12 w-col-4 w-col-medium-6 w-col-stack">
+<div class="link-depoimento w-inline-block">
+<div class="w-embed-youtubevideo">
+<div class="w-embed-youtubevideo-depo"> <iframe class="iframe-youtube" src="https://www.youtube.com/embed/UCfNip5lF1eEutTt-skja6iQ?autoplay=0&rel=0" allowfullscreen></iframe> </div>
+</div>
+<div class="depoimento-texto">
+<p>texto</p>
+</div>
+<div class="info-cliente">
+<div class="w-row">
+<div class="w-col w-col-3">
+<div class="foto-cliente" style="background-image:url(https://www.galaxcms.com.br/imgs_crud_comum/3316/Giuliana-20191203110554.jpg)"></div>
+</div>
+<div class="w-col w-col-9">
+<div class="bloco-info-cliente">
+<h3 class="titulo-de">Giuliana</h3>
+<div>Diretora na Green House Eventos</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div> <span class="clear-991 row w-row"></span><span class="clear-767 row w-row"></span><span class="clear-479 row w-row"></span>
+<div class="col-reset w-col w-col-tiny-12 w-col-small-12 w-col-4 w-col-medium-6 w-col-stack">
+<div class="link-depoimento w-inline-block">
+<div class="w-embed-youtubevideo">
+<div class="w-embed-youtubevideo-depo"> <iframe class="iframe-youtube" src="https://www.youtube.com/embed/UCfNip5lF1eEutTt-skja6iQ?autoplay=0&rel=0" allowfullscreen></iframe> </div>
+</div>
+<div class="depoimento-texto">
+<p>texto</p>
+</div>
+<div class="info-cliente">
+<div class="w-row">
+<div class="w-col w-col-3">
+<div class="foto-cliente" style="background-image:url(https://www.galaxcms.com.br/imgs_crud_comum/3316/Israel-20191203114920.jpg)"></div>
+</div>
+<div class="w-col w-col-9">
+<div class="bloco-info-cliente">
+<h3 class="titulo-de">Israel</h3>
+<div>Analista de Marketing na Assescont</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div> <span class="clear-desktop row w-row"></span><span class="clear-767 row w-row"></span><span class="clear-479 row w-row"></span> </div>
+</div>
+</section>
+<!-- end -->
+
+<!-- cta -->
+<section class="barra-nao-fixa">
+<div class="limite-site">
+<div class="row-80 w-row">
+<div class="col-reset w-col w-col-3 w-col-stack">
+<div class="bloco-chamada">
+<div class="text-block-8">Não perca mais tempo, <span class="destacado" data-ix="pop-up-orcamento">fale com um consultor</span></div>
+<div class="seta"></div>
+</div>
+</div>
+<div class="col-reset w-col w-col-3 w-col-stack">
+<div class="bloco-cornometro">
+<div id="cont"> </div>
+</div>
+</div>
+<div class="col-reset w-col w-col-3 w-col-stack">
+<a href="tel:5531996022548" class="bloco-contato linkPhoneDDD w-inline-block">
+<div class="titulo-contato">Ligue agora mesmo!</div>
+<div class="numero"> <i class="fa fa-phone"></i><span class="phoneDDD"> 99602-2548 </span> </div>
+</a>
+<a target="_blank" href="" class="link-wpp-script button corrige-topo oculto-desk w-button">
+<i class="fa fa-whatsapp"></i> Orçamento por Whatsapp </a>
+</div>
+<div class="col-reset w-col w-col-3 w-col-stack">
+<a target="_blank" href="" class="link-wpp-script button oculto-mo w-button">
+<i class="fa fa-whatsapp"></i> Orçamento por Whatsapp </a>
+</div>
+</div>
+</div>
+</section>
+<!-- end -->
+
+<!-- otimizacao06 -->
+<div class="secao s5">
+<div class="limite-site">
+<div class="row-2 w-row">
+<section class="col-reset w-col w-col-6">
+<div class="bloco-texto _2">
+<h2 class="h2">Só hoje</h2>
+<div class="div-block-2">
+<p class="paragraph">Criação de Site e Loja Virtual com</p>
+<p class="peco-produto">40% de desconto</p>
+</div>
+<div class="div-block-2">
+<p class="paragraph">20 Contas de Email Profissional</p>
+<p class="peco-produto">GRÁTIS</p>
+</div>
+<div class="div-block-2">
+<p class="paragraph">Configuração de SEO</p>
+<p class="peco-produto">GRÁTIS</p>
+</div>
+<div class="div-block-2">
+<p class="paragraph">Entrega em</p>
+<p class="peco-produto">3 dias</p>
+</div>
+</div>
+</section>
+<section class="col-reset w-col w-col-6">
+<div class="form-site w-form" data-ix="form-pulsando">
+
+<form action="" method="post">
+
+<div class="h2 titulo-form">Receba um orçamento em 24h</div>
+<label for="Nome" class="label">Nome*</label>
+<input type="text" class="input w-input" maxlength="100" name="fale[nome]" required >
+<label for="Telefone" class="label">Telefone*</label>
+<input type="tel" class="input w-input" maxlength="20" name="fale[telefone]" required >
+<label for="E-mail" class="label">E-mail*</label>
+<input type="email" class="input w-input" maxlength="75" name="fale[email]" required >
+<div class="w-row">
+<button type="submit" name="EnviaForm" class="bt-padrao w-button button-landing"> Solicitar orçamento </button> </div>
+</form>
+
+</div>
+</section>
+</div>
+<div class="text-consulte"> *Consulte as condições com o vendedor </div>
+</div> 
+
+<span id="timeToCont" class="none" data-time=""></span>
+</div>
+<!-- end -->
+
+<!-- barra fixa -->
+<div class="barra-fixa">
+<div class="limite-site">
+<div class="row-80 w-row">
+<div class="col-reset w-col w-col-3 w-col-stack hidden-mobile">
+<div class="bloco-chamada">
+<div class="text-block-8">Não perca mais tempo, <span class="destacado" data-ix="pop-up-orcamento">fale com um consultor</span></div>
+<div class="seta"></div>
+</div>
+</div>
+<div class="col-reset w-col w-col-3 w-col-stack hidden-mobile">
+<div class="bloco-cornometro">
+<div id="contFixed"> </div>
+</div>
+</div>
+<div class="col-reset w-col w-col-3 w-col-stack">
+<a href="tel:5531996022548" class="bloco-contato linkPhoneDDD w-inline-block">
+<div class="titulo-contato">Ligue agora mesmo!</div>
+<div class="numero"> <i class="fa fa-phone"></i> <span class="phoneDDD"> 99602-2548 </span> </div>
+</a>
+<a target="_blank" href="" class="link-wpp-script button corrige-topo oculto-desk w-button">
+<i class="fa fa-whatsapp"></i> Orçamento por Whatsapp </a>
+</div>
+<div class="col-reset w-col w-col-3 w-col-stack">
+<a target="_blank" href="" class="link-wpp-script button oculto-mo w-button">
+<i class="fa fa-whatsapp"></i> Orçamento por Whatsapp </a>
+</div>
+</div>
+</div>
+</div>
+<!-- end -->
+
+</main>
+
+<!-- pop up saída -->
+<div class="pop-up pop-up-saida">
+<div class="limite-site">
+<div class="w-row">
+<div class="col-reset w-col w-col-2"></div>
+<div class="col-reset w-col w-col-8">
+<div class="form-site for-or-amento w-form">
+
+<form  method="post" action="" >
+<div class="w-row">
+<div class="w-col w-col-6 w-col-stack">
+<div class="div-block-3">
+<div class="h2 titulo-form cor2">Segure seu cupom de 40% OFF</div>
+<p class="p-desc"> Faça seu cadastro e receba um cupom de 40% de desconto para usar quando quiser. </p>
+</div>
+</div>
+<div class="w-col w-col-6 w-col-stack">
+
+<div class="camposForm"> 
+
+<label for="NomeSaida" class="label v2">Nome*</label>
+ <input type="text" class="input w-input" maxlength="100" name="fale[nome]" required >
+
+ <label for="TelefoneSaida" class="label v2">Telefone*</label> 
+ <input type="tel" class="input w-input" maxlength="20" name="fale[telefone]" required > 
+ 
+ <label for="E-mailSaida" class="label v2">E-mail*</label>
+ <input type="text" class="input w-input" maxlength="75" name="fale[email]" required >
+<div>
+<input type="submit" value="Enviar" name="EnviaForm" class="bt-padrao w-button"> </div>
+</div>
+</div>
+</div>
+</form>
+</div><a href="#" class="sair sair-pop-up"><i class="fa fa-close"></i></a></div>
+<div class="col-reset w-col w-col-2"></div>
+</div>
+</div>
+</div>
+<!-- end -->
+
+<!-- pop up orçamento -->
+<div class="pop-up-orcamento">
+<div class="limite-site">
+<div class="w-row">
+<div class="col-reset w-col w-col-4"></div>
+<div class="col-reset w-col w-col-4">
+<div class="form-site w-form">
+
+<form method="post" action="" >
+
+<div class="h2 titulo-form text-center m-b-10">Receba um orçamento em 24h</div>
+
+<div class="camposForm">
+ <label for="NomeOrcamento" class="label">Nome*</label> 
+ <input type="text" class="input w-input" maxlength="100" name="fale[nome]"> 
+ 
+ <label for="TelefoneOrcamento" class="label">Telefone*</label>
+ <input type="tel" class="input w-input" maxlength="20" name="fale[telefone]"> 
+ 
+ <label for="E-mailOrcamento" class="label">E-mail*</label>
+ <input type="text" class="input w-input" maxlength="75" name="fale[email]">
+ 
+<div> <input type="submit" value="Enviar" name="EnviaForm" class="bt-padrao w-button">
+ </div>
+
+</div> 
+ </form>
+ 
+</div> 
+<a href="#" class="sair" data-ix="sair-pop-up-orcamento"><i class="fa fa-close"></i></a> 
+</div>
+<div class="col-reset w-col w-col-4"></div>
+</div>
+</div>
+</div>
+<!-- end -->
+
+<?php
+if (isset($_POST['EnviaForm'])) {
+session_start();
+require ("_formulario/alterar_esse_arquivo.php"); 
+require("_formulario/class.phpmailer.php"); 
+$dados = $_REQUEST['fale'];
+
+$mail = new PHPMailer();
+$mail->SetLanguage("br");
+$mail->IsSMTP();
+$mail->Host = "$smtp_endereco";
+//$mail->AddAddress("$seu_email"); 
+$mail->Subject = "Contato - Lp";
+$mail->AddAddress("leonardo@sitevelox.com.br","Contato Lp");
+$mail->IsHTML(true);		
+$mail->FromName = "Contato - Lp";
+$mail->From = "leonardo@sitevelox.com.br";
+$mail->Body = "
+  
+   <b>Nome:</b> ".$dados['nome']." <p>
+   <b>Email:</b> ".$dados['email']." <p>
+   <b>Telefone:</b> ".$dados['telefone'];
+
+$mail->SMTPAuth = true;
+$mail->Username = "$usuario_smtp"; 
+$mail->Password = "$senha_smtp"; 
+if(!$mail->Send())
+{ 
+echo "<script>alert('Ocorreram erros ao enviar o email, verifique sua conexao com a internet.');location.href=''</script>";
+exit; 
+}
+else
+{
+echo "<script>alert('Email Enviado com sucesso!');location.href=''</script>"; 
+exit; 
+}
+} 
+?>
+
+<footer class="rodape">
+<div class="rodape-sitevelox">
+<img src="images/sitevelox.png" alt="" width="150" class="logo-sitevelox">
+<div class="text-block">© 2020 - Desenvolvido por <a href="" class="link-rodape-sitevelox">SiteVelox</a> - <a href="" class="link-rodape-sitevelox">Criação de Sites Profissionais</a></div>
+</div>
+</footer>
+</body>
+<script src="js/jquery.js"></script>
+<script language="JavaScript">
+function click()
+{
+ if (event.button==2)
+ {
+// alert('Botão Direito do Mouse Desabilitado');
+ }
+}
+ 
+function keypresed()
+{
+ //alert('Esta página está com o teclado desabilitado');
+}
+ 
+document.onkeydown=keypresed;
+document.onmousedown=click;
+ 
+ 
+var mensagem="";
+function clickIE() {if (document.all) {(mensagem);return false;}}
+function clickNS(e) {if
+(document.layers||(document.getElementById&&!document.all)) {
+if (e.which==2||e.which==3) {(mensagem);return false;}}}
+if (document.layers) 
+{document.captureEvents(Event.MOUSEDOWN);document.onmousedown=clickNS;}
+else{document.onmouseup=clickNS;document.oncontextmenu=clickIE;}
+document.oncontextmenu=new Function("return false");
+</script>
