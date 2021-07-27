b1c3f22328f8103a40da9f5d871f20a29df62cd6
Leonardo Roldão <41723429+veloxsite@users.noreply.github.com>
2021-07-27 06:36:57 -0300

    senha

--- _formulario/alterar_esse_arquivo.php
+++ _formulario/alterar_esse_arquivo.php
@@ -2,6 +2,6 @@
 $smtp_endereco = "mail.sitevelox.com.br";
 $seu_email = "leonardo@sitevelox.com.br";
 $usuario_smtp = "leonardo@sitevelox.com.br";
-$senha_smtp = "1a73zaP02@2017"; 
+$senha_smtp = ""; 
 $mensagem_sucesso = "Enviado com sucesso!";
 ?>
